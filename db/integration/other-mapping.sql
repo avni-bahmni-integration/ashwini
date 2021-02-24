e05d867b822ae9a64e8d7b04c602b9c649c1d01e
Hiren Thacker <hithacker@gmail.com>
2021-02-24 14:29:13 +0530

    OpenMRS and integration metadata for Enrolment sync

--- db/integration/other-mapping.sql
+++ db/integration/other-mapping.sql
@@ -48,3 +48,28 @@ values ('PatientSubject', 'Concept', 'b09be3d4-644e-487b-bc1c-d1eea88de25d', 'Da
 
 insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value)
 values ('PatientSubject', 'Concept', '4e149d1e-13a0-40ca-a4ec-b9fe0c77170e', 'Gender');
+
+-- Enrolment
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', 'de2390a8-3faa-4036-bb65-54202b825569', 'Mother', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', '944812fe-9856-44f1-9d2c-49ced9fcf808', 'Child', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', '3cdb0394-850b-4980-85cc-18ca00111ebc', 'TB', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', 'f805ed46-581b-41d3-b952-5b8074bf999e', 'Chronic sickness', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', '11c94225-5d25-4547-b088-e8bf78713033', 'Sickle Cell', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', '68f4ee11-87dd-42d6-8753-cb61fe3bb2e5', 'Family Planning', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'Community_Enrolment_EncounterType', '8a7175ec-2a04-4f51-9a10-3080b34361ed', 'Mental Health', 'Encounter type in OpenMRS for community enrolment data in Avni');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value, about)
+values ('ProgramEnrolment', 'EnrolmentUUID_Concept', '1da2bd25-91f6-4235-9b13-bbb25209121c', null, 'External uuid is used to match entities after first save');
--- db/openmrs/other_metadata.sql
+++ db/openmrs/other_metadata.sql
@@ -8,7 +8,30 @@ call add_concept_abi('First name [Avni]', 'First name', 'Text', 'Misc', false, '
 call add_concept_abi('Last name [Avni]', 'Last name', 'Text', 'Misc', false, '6e62e978-b45a-4a3b-a5dc-00f81f99bb77');
 call add_concept_abi('Date of birth [Avni]', 'Date of birth', 'Date', 'Misc', false, 'b09be3d4-644e-487b-bc1c-d1eea88de25d');
 call add_concept_abi('Gender [Avni]', 'Gender', 'Text', 'Misc', false, '4e149d1e-13a0-40ca-a4ec-b9fe0c77170e');
+call add_concept_abi('Avni Enrolment UUID [Avni]', 'Avni Enrolment UUID', 'Text', 'Misc', false, '1da2bd25-91f6-4235-9b13-bbb25209121c');
+
 
 -- Encounter types
 insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
     values ('Community Registration', curdate(), '18c9a1d6-c4f5-4b64-8dbb-cf8ded8b9552', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('Mother Community Enrolment', curdate(), 'de2390a8-3faa-4036-bb65-54202b825569', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('Child Community Enrolment', curdate(), '944812fe-9856-44f1-9d2c-49ced9fcf808', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('TB Community Enrolment', curdate(), '3cdb0394-850b-4980-85cc-18ca00111ebc', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('Chronic sickness Community Enrolment', curdate(), 'f805ed46-581b-41d3-b952-5b8074bf999e', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('Sickle Cell Community Enrolment', curdate(), '11c94225-5d25-4547-b088-e8bf78713033', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('Family Planning Community Enrolment', curdate(), '68f4ee11-87dd-42d6-8753-cb61fe3bb2e5', 1, curdate(), 1);
+
+insert into encounter_type (name, date_created, uuid, changed_by, date_changed, creator)
+values ('Mental Health Community Enrolment [Avni]', curdate(), '8a7175ec-2a04-4f51-9a10-3080b34361ed', 1, curdate(), 1);
