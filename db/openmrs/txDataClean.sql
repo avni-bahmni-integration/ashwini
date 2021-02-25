8340045a508dc1d90faa3ed9425a6ec44d5aeec5
Hiren Thacker <hithacker@gmail.com>
2021-02-25 15:14:18 +0530

    Update openmrs txDataClean script to first delete obs dependent on previous version

--- db/openmrs/txDataClean.sql
+++ db/openmrs/txDataClean.sql
@@ -1,3 +1,4 @@
+delete from obs where date_created > '2020-12-01' and previous_version is not null;
 delete from obs where date_created > '2020-12-01' and obs_group_id is not null;
 delete from obs where date_created > '2020-12-01';
 delete from encounter_provider where date_created > '2020-12-01';
