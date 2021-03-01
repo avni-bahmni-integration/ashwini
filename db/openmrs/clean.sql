af90dc28e48b74e08ef7d573fc8cff95ac2e741f
Hiren Thacker <hithacker@gmail.com>
2021-03-01 14:08:37 +0530

    Add concept_numeric cleaning to openmrs clean script

--- db/openmrs/clean.sql
+++ db/openmrs/clean.sql
@@ -1,3 +1,4 @@
+delete from concept_numeric where concept_id in (select concept_id from concept where date_created > '2020-12-01');
 delete from concept_name where date_created > '2020-12-01';
 delete from concept_answer where date_created > '2020-12-01';
 delete from concept where date_created > '2020-12-01';
