e9d80e284e1d2660f60cb562b27428ba216bdff8
Hiren Thacker <hithacker@gmail.com>
2021-03-17 13:24:26 +0530

    Updated openmrs clean scripts to work with new changes

--- db/openmrs/clean.sql
+++ db/openmrs/clean.sql
@@ -1,7 +1,9 @@
 delete from concept_numeric where concept_id in (select concept_id from concept where date_created > '2020-12-01');
 delete from concept_name where date_created > '2020-12-01';
 delete from concept_answer where date_created > '2020-12-01';
+delete from concept_set where date_created > '2020-12-01';
 delete from concept where date_created > '2020-12-01';
 delete from encounter_type where date_created > '2020-12-01';
 
 DROP PROCEDURE IF EXISTS add_concept_abi;
+DROP FUNCTION IF EXISTS add_concept_abi_func;
--- db/openmrs/txDataClean.sql
+++ db/openmrs/txDataClean.sql
@@ -1,4 +1,21 @@
 delete from obs where date_created > '2020-12-01' and previous_version is not null;
+
+delete
+from obs
+where obs_group_id in (select *
+                       from (select obs_id
+                             from obs
+                             where obs_group_id in
+                                   (select *
+                                    from (select obs_id
+                                          from obs
+                                          where date_created > '2020-12-01'
+                                            and obs_group_id is not null) as temp)) as temp2);
+delete
+from obs
+where obs_group_id in
+      (select * from (select obs_id from obs where date_created > '2020-12-01' and obs_group_id is not null) as temp);
+
 delete from obs where date_created > '2020-12-01' and obs_group_id is not null;
 delete from obs where date_created > '2020-12-01';
 delete from encounter_provider where date_created > '2020-12-01';
