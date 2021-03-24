c4a539b642bbbbb068a7ebba374eb2573a1d2bf4
Hiren Thacker <hithacker@gmail.com>
2021-03-24 16:37:26 +0530

    Update openmrs txDataClean to delete visits

--- db/openmrs/txDataClean.sql
+++ db/openmrs/txDataClean.sql
@@ -19,4 +19,6 @@ where obs_group_id in
 delete from obs where date_created > '2020-12-01' and obs_group_id is not null;
 delete from obs where date_created > '2020-12-01';
 delete from encounter_provider where date_created > '2020-12-01';
+delete from visit_attribute where date_created > '2020-12-01';
+delete from visit where date_created > '2020-12-01';
 delete from encounter where date_created > '2020-12-01';
