34f48958946eefa9340f8e81a8fee93c1d284e7a
Vivek Singh <petmongrels@gmail.com>
2021-04-12 16:23:42 +0530

    first run sqls

---
+++ config/bahmni/first-run-encounters.sql
@@ -0,0 +1,10 @@
+select encounter.encounter_id, encounter.uuid
+from encounter
+         join encounter_type et on encounter.encounter_type = et.encounter_type_id
+         join patient p on encounter.patient_id = p.patient_id
+         join patient_identifier pi on p.patient_id = pi.patient_id
+where et.name in ('Consultation', 'LAB_RESULT')
+  and encounter.voided = false
+  and pi.identifier like 'TRI%'
+  and encounter.encounter_id > ?
+order by encounter.encounter_id asc
