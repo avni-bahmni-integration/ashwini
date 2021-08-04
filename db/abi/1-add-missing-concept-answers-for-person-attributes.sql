3d79c1f0607936b9dedfc75aa183eebbf38cb0b3
Vivek Singh <petmongrels@gmail.com>
2021-08-04 15:03:33 +0530

    missing concept answers

---
+++ db/abi/1-add-missing-concept-answers-for-person-attributes.sql
@@ -0,0 +1,52 @@
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value)
+values
+('Observation', 'Concept','718b4589-2a11-4355-b8dc-aa668a93e098', 'Negative [H]'),
+('Observation', 'Concept','7acfafa4-f19b-485e-97a7-c9e002dbe37a', 'Positive [H]'),
+('Observation', 'Concept','f2355233-c552-4cd6-802f-0c7c75221f03', 'Not Applicable [H]'),
+('Observation', 'Concept','d9fee149-7451-4f00-a675-befe8ff63a07', 'Paniya [H]'),
+('Observation', 'Concept','83a2fe6e-e01b-413d-a70c-1d53c326de66', 'Bettakurumba [H]'),
+('Observation', 'Concept','9bc8d308-e740-48bd-ac4e-aac1b509b52d', 'Kattunayaka [H]'),
+('Observation', 'Concept','8a2c574b-def6-4d54-96f5-f6a760b46d68', 'Mullukurumba [H]'),
+('Observation', 'Concept','c67c56d4-4e56-413d-bec4-ca733d3482de', 'Irula [H]'),
+('Observation', 'Concept','977d71a9-b13c-4757-b4a2-19588353b4a9', 'A negative [H]'),
+('Observation', 'Concept','ed159068-8f33-4143-8257-9d5bdf41ab49', 'A positive [H]'),
+('Observation', 'Concept','88c17105-9dc6-45eb-bb0f-ac8d29700f8d', 'AA [H]'),
+('Observation', 'Concept','5056aecb-f56e-435d-a389-16a842408337', 'AB negative [H]'),
+('Observation', 'Concept','20075176-4ce8-42cb-9af9-d5155a973164', 'AB positive [H]'),
+('Observation', 'Concept','0fe64012-9bc6-4f85-840c-9242d06f4a14', 'AS [H]'),
+('Observation', 'Concept','9e5b3e38-67f0-4407-b090-6e31ef2af33a', 'B negative [H]'),
+('Observation', 'Concept','324f4ebd-25a8-45b1-a395-fa1322223d24', 'B Positive [H]'),
+('Observation', 'Concept','12ae6c85-dc0f-4964-9a42-d841adce8e45', 'O negative [H]'),
+('Observation', 'Concept','aa251495-b536-42e3-8bcc-55000b0b0bf9', 'O Positive [H]'),
+('Observation', 'Concept','2efa1e6e-4710-46b3-936a-b4b06a3d0d1f', 'SS [H]'),
+('Observation', 'Concept','44625a00-d08e-43d4-b590-61e37c49b59d', 'Not Performed [H]'),
+('Observation', 'Concept','845126fc-9267-476d-8f9a-d215a5a08f41', 'Sickle Cell Patients [H]'),
+('Observation', 'Concept','29034aba-2ece-415a-a509-ffbec18c50c3', 'Staffs [H]'),
+('Observation', 'Concept','830f757c-02ec-416c-a23b-f04f9ca4ef4e', 'General Patients [H]');
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value)
+values
+('Observation', 'Concept', '7c8c16bb-ee03-4e63-a15c-6fe86db235e2', 'Departments, General OPD [H]'),
+('Observation', 'Concept', '65209998-1506-42e0-b0cc-6aa04f1a0a30', 'Departments, Obstetrics and Gynecology [H]'),
+('Observation', 'Concept', '2c619057-bebd-4ac3-999e-a35757fe77f6', 'Departments, ENT [H]'),
+('Observation', 'Concept', '08a120da-f84e-4f8f-849a-be2a589d4dfb', 'Departments, Orthopedic [H]'),
+('Observation', 'Concept', '2d251c03-f910-418c-a992-29aa544f69e8', 'Departments, Paediatrics [H]'),
+('Observation', 'Concept', '56a17ddd-f441-4b57-92d5-788b1fee9cdb', 'Departments, Cardiology [H]'),
+('Observation', 'Concept', 'ca2cbaaf-8d66-42ef-b6df-0463794fb609', 'Departments, Vascular [H]'),
+('Observation', 'Concept', 'c1e710d1-3dde-4c76-a74e-446f7ff38f43', 'Departments, Dermatology [H]'),
+('Observation', 'Concept', '1cff782d-b8a0-4589-83cc-e8427d3c24a3', 'Departments, Disabled [H]'),
+('Observation', 'Concept', '50a6914c-6910-49dd-9ad2-d33efe99a066', 'Departments, Dental [H]'),
+('Observation', 'Concept', '3d809fe4-6d2c-47ee-9ade-50c7a57725a9', 'Departments, Psychiatric [H]'),
+('Observation', 'Concept', '7d3d2bdf-5b3c-4937-ae0d-33009fec948a', 'Departments, Urology [H]'),
+('Observation', 'Concept', '771b7eb7-9fe2-4af0-810e-34545eebc36c', 'Departments, Medicine Only [H]'),
+('Observation', 'Concept', '35d87700-5895-4f52-9c1d-bce8ca98951c', 'Departments, Lab Results only [H]'),
+('Observation', 'Concept', '92bc2bdd-3163-49f7-8f98-0b9ad9b3e369', 'Departments, SCAN [H]'),
+('Observation', 'Concept', '4bf7fd0e-9298-442b-bd3b-a350c9b4bb99', 'Departments, Endoscopy [H]'),
+('Observation', 'Concept', 'd0da2b2c-af94-47cf-a256-5d4cab62287b', 'Departments, Followup [H]'),
+('Observation', 'Concept', '0548da4d-3725-41e8-8c0e-4c944956c1b4', 'Departments, Physiotherapy [H]'),
+('Observation', 'Concept', '8e1cca5f-e05e-4ea7-ba45-b731a4665a13', 'Departments, General Surgery [H]');
+
+
+insert into mapping_metadata (mapping_group_name, mapping_name, bahmni_value, avni_value)
+values
+('Observation', 'Concept', '41207c87-d18d-4335-88dd-dc63f305a779', 'Other occupation [H]');
