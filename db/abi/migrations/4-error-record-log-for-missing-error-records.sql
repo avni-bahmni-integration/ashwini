63188f4971ad79912807644e0b91e5a74999c6b1
Vivek Singh <petmongrels@gmail.com>
2021-08-13 17:45:11 +0530

    error record log for new error records

--- db/abi/migrations/4-error-record-log-for-missing-error-records.sql
+++ db/abi/migrations/4-error-record-log-for-missing-error-records.sql
@@ -1,2 +1,2 @@
 insert into error_record_log (error_type, logged_at, error_record_id)
-select 'MultipleSubjectsWithId', current_time, id from error_record where id > 2279;
+select 'MultipleSubjectsWithId', now() - (id * interval '1 second'), id from error_record where id > 2279;
