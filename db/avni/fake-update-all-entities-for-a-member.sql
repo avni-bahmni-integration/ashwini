f0922f1eed4c2aae6970b2e0b96625fa4e951a8e
Vivek Singh <petmongrels@gmail.com>
2021-09-23 15:45:56 +0530

    fake-update-all-entities-for-a-member

--- db/avni/fake-update-all-entities-for-a-member.sql
+++ db/avni/fake-update-all-entities-for-a-member.sql
@@ -1,35 +1,55 @@
 set role ashwini;
-select current_timestamp + (individual.id * interval '100 second')/(select max(id) from individual)
+select current_timestamp + (individual.id * interval '100 second')/(select max(id) from individual),
+       individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
     from individual
     join audit on individual.audit_id = audit.id
     where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
-        '11450708',
-        '05020310',
-        '05180706',
-        '05182314') and individual.is_voided = false
+        '08110307',
+        '08110308',
+        '08592112',
+        '10290809',
+        '10361710') and individual.is_voided = false
 order by 1;
-select current_timestamp + (program_enrolment.id * interval '100 second')/(select max(id) from program_enrolment)
+
+select current_timestamp + (program_enrolment.id * interval '100 second')/(select max(id) from program_enrolment),
+       individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
         from program_enrolment
          join audit on program_enrolment.audit_id = audit.id
          join individual on program_enrolment.individual_id = individual.id
-where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
-                                                                             '11450708',
-                                                                             '05020310',
-                                                                             '05180706',
-                                                                             '05182314') and individual.is_voided = false
+where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('08110307',
+                                                                             '08110308',
+                                                                             '08592112',
+                                                                             '10290809',
+                                                                             '10361710'
+                                                                             ) and individual.is_voided = false
 order by 1;
-select current_timestamp + (program_encounter.id * interval '1000 second')/(select max(id) from program_encounter)
+
+select current_timestamp + (program_encounter.id * interval '10000 second')/(select max(id) from program_encounter),
+       individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
         from program_encounter
          join audit on program_encounter.audit_id = audit.id
         join program_enrolment on program_encounter.program_enrolment_id = program_enrolment.id
             join individual on program_enrolment.individual_id = individual.id
 where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
-                                                                             '11450708',
-                                                                             '05020310',
-                                                                             '05180706',
-                                                                             '05182314') and individual.is_voided = false
+                                                                             '08110307',
+                                                                             '08110308',
+                                                                             '08592112',
+                                                                             '10290809',
+                                                                             '10361710') and individual.is_voided = false
 order by 1;
 
+select current_timestamp + (encounter.id * interval '1000 second')/(select max(id) from encounter),
+       individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
+from encounter
+         join audit on encounter.audit_id = audit.id
+         join individual on encounter.individual_id = individual.id
+where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
+                                                                             '08110307',
+                                                                             '08110308',
+                                                                             '08592112',
+                                                                             '10290809',
+                                                                             '10361710') and individual.is_voided = false
+order by 1;
 
 
 start transaction;
@@ -39,11 +59,11 @@ set last_modified_date_time = current_timestamp + (individual.id * interval '100
 from individual
 where individual.audit_id = audit.id
   and individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
-                                                                             '11450708',
-                                                                             '05020310',
-                                                                             '05180706',
-                                                                             '05182314'
-    ) and individual.is_voided = false;
+                                                                             '08110307',
+                                                                             '08110308',
+                                                                             '08592112',
+                                                                             '10290809',
+                                                                             '10361710') and individual.is_voided = false;
 commit;
 
 start transaction;
@@ -53,26 +73,26 @@ set last_modified_date_time = current_timestamp + (program_enrolment.id * interv
 from individual, program_enrolment
 where program_enrolment.audit_id = audit.id
   and program_enrolment.individual_id = individual.id
-  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
-                                                                           '11450708',
-                                                                           '05020310',
-                                                                           '05180706',
-                                                                           '05182314'
+  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('08110307',
+                                                                           '08110308',
+                                                                           '08592112',
+                                                                           '10290809',
+                                                                           '10361710'
     ) and individual.is_voided = false;
 commit;
 
 start transaction;
 update audit
-set last_modified_date_time = current_timestamp + (program_encounter.id * interval '100 second')/(select max(id) from program_encounter),
+set last_modified_date_time = current_timestamp + (program_encounter.id * interval '10000 second')/(select max(id) from program_encounter),
     last_modified_by_id = 3144
 from individual, program_enrolment, program_encounter
 where program_encounter.audit_id = audit.id
   and program_enrolment.individual_id = individual.id
   and program_enrolment.id = program_encounter.program_enrolment_id
-  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in (
-                                                                           '11450708',
-                                                                           '05020310',
-                                                                           '05180706',
-                                                                           '05182314'
+  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('08110307',
+                                                                           '08110308',
+                                                                           '08592112',
+                                                                           '10290809',
+                                                                           '10361710'
 ) and individual.is_voided = false;
 commit;
