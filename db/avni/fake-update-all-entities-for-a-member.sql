ea45412fba566cec82c5c1269a59f7eb9cad044b
Vivek Singh <petmongrels@gmail.com>
2021-11-08 11:01:15 +0530

    history

--- db/avni/fake-update-all-entities-for-a-member.sql
+++ db/avni/fake-update-all-entities-for-a-member.sql
@@ -3,43 +3,33 @@ select current_timestamp + (individual.id * interval '200 second')/(select max(i
        individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
     from individual
     join audit on individual.audit_id = audit.id
-    where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                                 '09034206',
-                                                                                 '09033508',
-                                                                                 '09060104',
-                                                                                 '09060105',
-                                                                                 '09350608',
-                                                                                 '09351906',
-                                                                                 '07041015',
-                                                                                 '07041016',
-                                                                                 '07045503',
-                                                                                 '08031007',
-                                                                                 '08461611',
-                                                                                 '08480707',
-                                                                                 '09342813',
-                                                                                 '10370306') and individual.is_voided = false
+    where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                                 '03062103',
+                                                                                 '03060207',
+                                                                                 '03060208',
+                                                                                 '03062104',
+                                                                                 '03072408',
+                                                                                 '03111306',
+                                                                                 '03112405',
+                                                                                 '03150609',
+                                                                                 '03241609') and individual.is_voided = false
 order by 1;
 
-select current_timestamp + (program_enrolment.id * interval '200 second')/(select max(id) from program_enrolment),
+select current_timestamp + (program_enrolment.id * interval '1000 second')/(select max(id) from program_enrolment),
        individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
         from program_enrolment
          join audit on program_enrolment.audit_id = audit.id
          join individual on program_enrolment.individual_id = individual.id
-where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                             '09034206',
-                                                                             '09033508',
-                                                                             '09060104',
-                                                                             '09060105',
-                                                                             '09350608',
-                                                                             '09351906',
-                                                                             '07041015',
-                                                                             '07041016',
-                                                                             '07045503',
-                                                                             '08031007',
-                                                                             '08461611',
-                                                                             '08480707',
-                                                                             '09342813',
-                                                                             '10370306') and individual.is_voided = false
+where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                             '03062103',
+                                                                             '03060207',
+                                                                             '03060208',
+                                                                             '03062104',
+                                                                             '03072408',
+                                                                             '03111306',
+                                                                             '03112405',
+                                                                             '03150609',
+                                                                             '03241609') and individual.is_voided = false
 order by 1;
 
 select current_timestamp + (program_encounter.id * interval '10000 second')/(select max(id) from program_encounter),
@@ -48,21 +38,16 @@ select current_timestamp + (program_encounter.id * interval '10000 second')/(sel
          join audit on program_encounter.audit_id = audit.id
         join program_enrolment on program_encounter.program_enrolment_id = program_enrolment.id
             join individual on program_enrolment.individual_id = individual.id
-where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                             '09034206',
-                                                                             '09033508',
-                                                                             '09060104',
-                                                                             '09060105',
-                                                                             '09350608',
-                                                                             '09351906',
-                                                                             '07041015',
-                                                                             '07041016',
-                                                                             '07045503',
-                                                                             '08031007',
-                                                                             '08461611',
-                                                                             '08480707',
-                                                                             '09342813',
-                                                                             '10370306') and individual.is_voided = false
+where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                             '03062103',
+                                                                             '03060207',
+                                                                             '03060208',
+                                                                             '03062104',
+                                                                             '03072408',
+                                                                             '03111306',
+                                                                             '03112405',
+                                                                             '03150609',
+                                                                             '03241609') and individual.is_voided = false
 order by 1;
 
 select current_timestamp + (encounter.id * interval '1000 second')/(select max(id) from encounter),
@@ -70,21 +55,16 @@ select current_timestamp + (encounter.id * interval '1000 second')/(select max(i
 from encounter
          join audit on encounter.audit_id = audit.id
          join individual on encounter.individual_id = individual.id
-where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                             '09034206',
-                                                                             '09033508',
-                                                                             '09060104',
-                                                                             '09060105',
-                                                                             '09350608',
-                                                                             '09351906',
-                                                                             '07041015',
-                                                                             '07041016',
-                                                                             '07045503',
-                                                                             '08031007',
-                                                                             '08461611',
-                                                                             '08480707',
-                                                                             '09342813',
-                                                                             '10370306') and individual.is_voided = false
+where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                             '03062103',
+                                                                             '03060207',
+                                                                             '03060208',
+                                                                             '03062104',
+                                                                             '03072408',
+                                                                             '03111306',
+                                                                             '03112405',
+                                                                             '03150609',
+                                                                             '03241609') and individual.is_voided = false
 order by 1;
 
 
@@ -94,21 +74,16 @@ set last_modified_date_time = current_timestamp + (individual.id * interval '100
     last_modified_by_id = 3144
 from individual
 where individual.audit_id = audit.id
-  and individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                             '09034206',
-                                                                             '09033508',
-                                                                             '09060104',
-                                                                             '09060105',
-                                                                             '09350608',
-                                                                             '09351906',
-                                                                             '07041015',
-                                                                             '07041016',
-                                                                             '07045503',
-                                                                             '08031007',
-                                                                             '08461611',
-                                                                             '08480707',
-                                                                             '09342813',
-                                                                             '10370306') and individual.is_voided = false;
+  and individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                             '03062103',
+                                                                             '03060207',
+                                                                             '03060208',
+                                                                             '03062104',
+                                                                             '03072408',
+                                                                             '03111306',
+                                                                             '03112405',
+                                                                             '03150609',
+                                                                             '03241609') and individual.is_voided = false;
 commit;
 
 start transaction;
@@ -118,21 +93,16 @@ set last_modified_date_time = current_timestamp + (program_enrolment.id * interv
 from individual, program_enrolment
 where program_enrolment.audit_id = audit.id
   and program_enrolment.individual_id = individual.id
-  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                           '09034206',
-                                                                           '09033508',
-                                                                           '09060104',
-                                                                           '09060105',
-                                                                           '09350608',
-                                                                           '09351906',
-                                                                           '07041015',
-                                                                           '07041016',
-                                                                           '07045503',
-                                                                           '08031007',
-                                                                           '08461611',
-                                                                           '08480707',
-                                                                           '09342813',
-                                                                           '10370306') and individual.is_voided = false;
+  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                           '03062103',
+                                                                           '03060207',
+                                                                           '03060208',
+                                                                           '03062104',
+                                                                           '03072408',
+                                                                           '03111306',
+                                                                           '03112405',
+                                                                           '03150609',
+                                                                           '03241609') and individual.is_voided = false;
 commit;
 
 start transaction;
@@ -143,19 +113,14 @@ from individual, program_enrolment, program_encounter
 where program_encounter.audit_id = audit.id
   and program_enrolment.individual_id = individual.id
   and program_enrolment.id = program_encounter.program_enrolment_id
-  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('09032705',
-                                                                           '09034206',
-                                                                           '09033508',
-                                                                           '09060104',
-                                                                           '09060105',
-                                                                           '09350608',
-                                                                           '09351906',
-                                                                           '07041015',
-                                                                           '07041016',
-                                                                           '07045503',
-                                                                           '08031007',
-                                                                           '08461611',
-                                                                           '08480707',
-                                                                           '09342813',
-                                                                           '10370306') and individual.is_voided = false;
+  and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('03060207',
+                                                                           '03062103',
+                                                                           '03060207',
+                                                                           '03060208',
+                                                                           '03062104',
+                                                                           '03072408',
+                                                                           '03111306',
+                                                                           '03112405',
+                                                                           '03150609',
+                                                                           '03241609') and individual.is_voided = false;
 commit;
