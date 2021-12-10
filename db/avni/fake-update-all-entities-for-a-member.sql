9d8e045e4549d87890b6e3177c11579ea15b0f81
Vivek Singh <petmongrels@gmail.com>
2021-12-10 15:07:15 +0530

    changed last modified date column

--- db/avni/fake-update-all-entities-for-a-member.sql
+++ db/avni/fake-update-all-entities-for-a-member.sql
@@ -1,8 +1,8 @@
+rollback;
 set role ashwini;
 select current_timestamp + (individual.id * interval '1000 second')/(select max(id) from individual),
        individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
     from individual
-    join audit on individual.audit_id = audit.id
     where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
                                                                                  '01098702',
                                                                                  '01100408',
@@ -21,7 +21,6 @@ order by 1;
 select current_timestamp + (program_enrolment.id * interval '1000 second')/(select max(id) from program_enrolment),
        individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
         from program_enrolment
-         join audit on program_enrolment.audit_id = audit.id
          join individual on program_enrolment.individual_id = individual.id
 where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
                                                                              '01098702',
@@ -41,7 +40,6 @@ order by 1;
 select current_timestamp + (program_encounter.id * interval '10000 second')/(select max(id) from program_encounter),
        individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
         from program_encounter
-         join audit on program_encounter.audit_id = audit.id
         join program_enrolment on program_encounter.program_enrolment_id = program_enrolment.id
             join individual on program_enrolment.individual_id = individual.id
 where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
@@ -62,7 +60,6 @@ order by 1;
 select current_timestamp + (encounter.id * interval '1000 second')/(select max(id) from encounter),
        individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce'
 from encounter
-         join audit on encounter.audit_id = audit.id
          join individual on encounter.individual_id = individual.id
 where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
                                                                              '01098702',
@@ -81,12 +78,10 @@ order by 1;
 
 
 start transaction;
-update audit
+update individual
 set last_modified_date_time = current_timestamp + (individual.id * interval '100 second')/(select max(id) from individual),
     last_modified_by_id = 3144
-from individual
-where individual.audit_id = audit.id
-  and individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
+where individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
                                                                              '01098702',
                                                                              '01100408',
                                                                              '01100606',
@@ -102,12 +97,11 @@ where individual.audit_id = audit.id
 commit;
 
 start transaction;
-update audit
+update program_enrolment
 set last_modified_date_time = current_timestamp + (program_enrolment.id * interval '100 second')/(select max(id) from program_enrolment),
     last_modified_by_id = 3144
-from individual, program_enrolment
-where program_enrolment.audit_id = audit.id
-  and program_enrolment.individual_id = individual.id
+from individual
+where program_enrolment.individual_id = individual.id
   and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
                                                                            '01098702',
                                                                            '01100408',
@@ -124,12 +118,11 @@ where program_enrolment.audit_id = audit.id
 commit;
 
 start transaction;
-update audit
+update program_encounter
 set last_modified_date_time = current_timestamp + (program_encounter.id * interval '10000 second')/(select max(id) from program_encounter),
     last_modified_by_id = 3144
-from individual, program_enrolment, program_encounter
-where program_encounter.audit_id = audit.id
-  and program_enrolment.individual_id = individual.id
+from individual, program_enrolment
+where program_enrolment.individual_id = individual.id
   and program_enrolment.id = program_encounter.program_enrolment_id
   and individual.observations->>'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' in ('06041204',
                                                                            '01098702',
