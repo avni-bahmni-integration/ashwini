86472b3a72e0c3f19678cd374493618ce1e9cb50
Vivek Singh <petmongrels@gmail.com>
2021-12-15 12:25:58 +0530

    last two catchments

--- db/avni/extract-one-catchment.sql
+++ db/avni/extract-one-catchment.sql
@@ -1,16 +1,18 @@
 -- Gudalur, Pattavayal
 set role ashwini;
+select name from catchment order by 1;
+
 select *
 from users
          join catchment on users.catchment_id = catchment.id
-where catchment.name in ('Devala', 'Srimadurai');
+where catchment.name in ('Devarshola', 'Erumadu');
 
 select distinct concat('TRI', individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce')
 from catchment
          join catchment_address_mapping on catchment.id = catchment_address_mapping.catchment_id
          join address_level on catchment_address_mapping.addresslevel_id = address_level.id
          join individual on individual.address_id = address_level.id
-where catchment.name in ('Devala', 'Srimadurai')
+where catchment.name in ('Devarshola', 'Erumadu')
   and individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' is not null
   and individual.observations ->> 'c9e64f06-23ac-47dc-b7e4-c93b819f52ce' != '';
 
