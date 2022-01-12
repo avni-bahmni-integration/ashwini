28f6ed53e0d125b104d29a1ba16af6ac6bdaed8c
Vivek Singh <petmongrels@gmail.com>
2022-01-12 14:40:57 +0530

    lab results worker data

---
+++ db/abi/migrations/done/2022-01-12-bahmni-entity-status.sql
@@ -0,0 +1,1 @@
+insert into bahmni_entity_status (entity_type, read_upto) VALUES ('LabResult', 667194);
---
+++ db/abi/migrations/done/7-marker-update.sql
@@ -0,0 +1,4 @@
+insert into markers (feed_uri, feed_uri_for_last_read_entry, last_read_entry_id) values ('http://localhost:8050/openmrs/ws/atomfeed/patient/recent', 'http://lo
+calhost:8050/openmrs/ws/atomfeed/patient/49450', 'tag:atomfeed.ict4h.org:39856f2c-d0b9-4656-a0b6-307f002190f9');
+insert into markers (feed_uri, feed_uri_for_last_read_entry, last_read_entry_id) values ('http://localost:8050/openmrs/ws/atomfeed/encounter/recent', 'http://l
+ocalhost:8050/openmrs/ws/atomfeed/encounter/210170', 'tag:atomfeed.ict4h.org:c626d7d3-2b50-4070-809e-31b3998a4abb');
